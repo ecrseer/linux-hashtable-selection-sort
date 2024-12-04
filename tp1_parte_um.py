0c33ee630562226df3194e39e5bc08ae3f963e5b
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-12-04 12:27:58 -0300

    estimando tempo com graficos e memoria hash fila pilha

--- tp1_parte_um.py
+++ tp1_parte_um.py
@@ -1,11 +1,16 @@
 import time
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
 
 
 def tp1_parte_um():
     caminho_lista_arquivos = "listagem_arquivos.txt"
     lista_arquivos = ler_lista_de_arquivos(caminho_lista_arquivos)
-    mostra_media_bubble_sort(lista_arquivos[:700])
-    mostra_media_quick_sort(lista_arquivos[:700])
+    bubble = mostra_media_bubble_sort(lista_arquivos[:700])
+    quick = mostra_media_quick_sort(lista_arquivos[:700])
+    criar_grafico(lista_arquivos)
 
 
 def ler_lista_de_arquivos(caminho_arquivo):
@@ -24,10 +29,11 @@ def mostra_media_bubble_sort(lista_arquivos):
     print(f"""Bubble Sort - Ordenando lista de arquivos:
     Média de tempo: {media_tempo} segundos.
     -------------------""")
+    return media_tempo
 
 
 class BubbleSort:
-    tempos = [time.time()]
+    linha_do_tempo = [time.time()]
 
     def ordenar_bubble_sort(self, file_path, lista_desordenada):
         tempo_inicio = time.time()
@@ -54,7 +60,7 @@ class BubbleSort:
             if lista[i] < lista[i + 1]:
                 lista[i], lista[i + 1] = lista[i + 1], lista[i]
                 atual = time.time()
-                self.tempos.append(atual - self.tempos[-1])
+                self.linha_do_tempo.append(atual - self.linha_do_tempo[-1])
                 tudo_ordenado = False
         return lista, tudo_ordenado
 
@@ -69,6 +75,7 @@ def mostra_media_quick_sort(lista_arquivos):
     print(f"""Quick Sort - Ordenando lista de arquivos:
     Média de tempo: {media_tempo} segundos.
     -------------------""")
+    return media_tempo
 
 
 class QuickSort:
@@ -107,3 +114,33 @@ class SelectionSort:
             for j in range(i + 1, len(lista)):
                 if lista[j] < lista[menor]:
                     menor = j
+
+
+def criar_grafico(lista_arquivos):
+    tamanho_lista = [30, 70, 200, 500, 700]
+    tempos_bubble = []
+    tempos_quick = []
+    for tamanho in tamanho_lista:
+        bubble, quick = gera_dados_grafico(lista_arquivos,tamanho)
+        tempos_bubble.append(bubble)
+        tempos_quick.append(quick)
+
+    print(f"tamanho qq {len(tempos_quick)}")
+    print(f"tamanho bb {len(tempos_bubble)}")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(tamanho_lista, tempos_bubble, label="Bubble Sort", marker='x')
+    plt.plot(tamanho_lista, tempos_quick, label="Quick Sort", marker='o')
+    plt.xlabel("Array Size (Power of 2)")
+    plt.ylabel("Execution Time")
+    plt.title("Execution Time Comparison")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    plt.savefig("plot.png")
+
+
+def gera_dados_grafico(lista_arquivos, tamanho=700):
+    bubble = mostra_media_bubble_sort(lista_arquivos[:tamanho])
+    quick = mostra_media_quick_sort(lista_arquivos[:tamanho])
+    return bubble, quick
